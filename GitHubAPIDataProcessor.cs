dfef9690c13dbf0762a45de5432de691018fc921
suny-am <visualarea.1@gmail.com>
2024-04-12 07:55:16 +0000

    Add relevant packages for CosmosDB

--- GitHubAPIDataProcessor.cs
+++ GitHubAPIDataProcessor.cs
@@ -1,4 +1,6 @@
 using System;
+using Azure.Identity;
+using Microsoft.Azure.Cosmos;
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.Logging;
 
@@ -7,20 +9,34 @@ namespace gh_data_collector
     public class GitHubAPIDataProcessor
     {
         private readonly ILogger _logger;
+        private readonly CosmosClient _client;
 
         public GitHubAPIDataProcessor(ILoggerFactory loggerFactory)
         {
+            var credential = new DefaultAzureCredential();
             _logger = loggerFactory.CreateLogger<GitHubAPIDataProcessor>();
+            _client = new(
+                accountEndpoint: Environment.GetEnvironmentVariable("COSMOS_ENDPOINT"),
+                tokenCredential: credential
+            );
         }
 
-        [Function("GitHubAPIDataProcessor")]
-        public void Run([TimerTrigger("0 */5 * * * *")] TimerInfo myTimer)
+        [Function("GithubAPIWrite")]
+        // Run trigger at 22:00
+        public void Run([TimerTrigger("0 0 22 * * *")] TimerInfo myTimer)
         {
-            _logger.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-            
+
+            // Create DB if not exists
+
+            // Create Container if not exists
+
+            // Create Item if not exists
+
+            _logger.LogInformation("{Message}", $"C# Timer trigger function executed at: {DateTime.Now}");
+
             if (myTimer.ScheduleStatus is not null)
             {
-                _logger.LogInformation($"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
+                _logger.LogInformation("{Message}", $"Next timer schedule at: {myTimer.ScheduleStatus.Next}");
             }
         }
     }
